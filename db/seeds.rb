63d42324beb2b25c565e37e2468e147fe788ee31
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-19 10:31:57 +0100

    Home added - Ready to code

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "Cleaning Database..."
+
+# db/seeds.rb
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+puts "Seeds Done 👍"
