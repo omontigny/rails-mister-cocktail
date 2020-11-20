2e14e8863c21c95f7d2b2db2a3e23efebce5332c
Olivier Montigny <olivier.wagon498@free.fr>
2020-11-20 13:50:38 +0100

    Correction route vers homepage

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   # get 'pages/home'
   root to: 'cocktails#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :ingredients, only: [:index]
-  resources :cocktails, only: [:index, :show, :new, :create] do
+  # resources :ingredients, only: [:index]
+  resources :cocktails, only: [:show, :new, :create] do
     resources :doses, only: [:new, :create]
   end
 
